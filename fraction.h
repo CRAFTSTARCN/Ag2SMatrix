c287fa8bd272d2a714a3bff1d40e3211e0853fd4
CRAFTSTARCN <179680737@qq.com>
2020-01-07 17:22:28 +0800

    Fraction2.0 Rewrite the class fix the value bug

--- fraction.h
+++ fraction.h
@@ -1,144 +1,164 @@
-//Fraction
 /*
-Powered By Ag2S at SDU
-Just a fleshman learner =-=
-so the lib has bug please inform me at 
-179608737@qq.com(I got a school email but that would expose my student number)
-thanks for using
+AG2S FRACTION 2.0
+A learning project of a freshman
+*Fixed some bug about value
 */
+
 #pragma once
 #ifndef __AG2S__FRACTION
 #define __AG2S__FRACTION
 
 #ifndef __cplusplus
-#error C++ Liburary
+#error Please use C++
 #endif
 
-#if __cplusplus < 201100LL  //if you're using Visual studio, delete the fowing lines line
-#error Low_Edition
+#if __cplusplus <201100L
+#error Low edition
 #endif
+#define LL long long
 
-#include <iostream>
 #include <cmath>
-#include <string>
+#include <iostream>
 using namespace std;
-
-int got_max_spanner(int i,int j) {
+int gcd(int i,int j) {
 	int a=i,b=j;
 	if(b!=0) {
-		return got_max_spanner(b,a%b);
+		return gcd(b,a%b);
 	} else {
 		return a;
 	}
 }
 
+//defination of the class;
 class fraction {
-	private:
-		int molecular;
-		int denominator;
-		double value;
+    private:
+    LL molecular;
+    LL denominator;
 	public:
-		fraction (int up,int down) {
-			molecular = up;
-			denominator = down;
-			reduction();
-			value = (double)up/(double)down;
-		}
-		fraction () = default;
-		fraction (double val) { /*A funny way to turn double to fraction*/
-			value = val;
-			int ti=val;
-			double td = val-ti;
-			int times = 1;
-			while (td > 0){
-				val *= 10;
-				ti = val;
-				td= val - ti;
-				times *=10; 
-			}
-			molecular = ti;
-			denominator = times;
-			reduction();
-		}
-		fraction (int val) {
-			value = val;
-			molecular = val;
-			denominator = 1;
-		}
-		~fraction () = default;
-		int mole () const { return molecular;}
-		int deno () const { return denominator;}
-		double val() const { return value;}
-
-		void reduction () {
-			int span = got_max_spanner(molecular,denominator);
-			if(span != 0){
-				molecular = molecular/span;
-				denominator = denominator/span;
-			}
-		}
-		void output () {
-			if(denominator == 1 || molecular == 0)
-				cout<<molecular;
-			else
-				cout<<molecular<<'/'<<denominator;
-		}
-		fraction operator+ (const fraction &c) {
-			fraction b;
-			b.denominator = this->denominator*c.denominator;
-			b.molecular= this->molecular*c.denominator+c.molecular*this->denominator;
-			b.value =this->value+c.value;
-			b.reduction();
-			return b;
-		}
-		fraction operator- (const fraction &c) {
-			fraction b;
-			b.denominator = this->denominator*c.denominator;
-			b.molecular= this->molecular*c.denominator-c.molecular*this->denominator;
-			b.value =this->value-c.value;
-			b.reduction();
-			return b;
-		}
-		fraction operator* (const fraction &c) {
-			fraction b;
-			b.denominator = this->denominator*c.denominator;
-			b.molecular =this->molecular*c.molecular;
-			b.value = this->value*c.value;
-			b.reduction();
-			return b;
-		}
-		fraction operator/ (const fraction &c) {
-			fraction b;
-			fraction temp;
-			temp.molecular = c.denominator;
-			temp.denominator = c.molecular;
-			b=*this*temp;
-			return b;
-		}
-		/*The default = operator between fractions works*/
-		fraction operator= (const int &i) {
-			this->denominator = 1;
-			this->molecular = i;
-			this->value = i;
-			return *(this);
-		}
-		fraction operator= (const double &d) {
-			this->value = d;
-			double val = d;
-			int ti = val;
-			double td = val-ti;
-			int times = 1;
-			while (td > 0){
-				val *= 10;
-				ti = val;
-				td= val - ti;
-				times *=10; 
-			}
-			this -> molecular = ti;
-			this ->denominator = times;
-			this ->reduction();
-			return *(this);
-		}
+	LL mole() const {return molecular;}
+	LL deno() const {return denominator;}
+	double val () const {return double(molecular)/double(denominator);}
+	fraction() = default;
+	fraction(LL,LL);
+	fraction(double);
+	~fraction() = default;
+	void reduction();
+	bool operator== (const fraction);
+	bool operator>  (const fraction);
+	bool operator<  (const fraction);
+	fraction operator+ (const fraction&);
+	fraction operator- (const fraction&);
+	fraction operator* (const fraction&);
+	fraction operator/ (const fraction&);
 };
+//declaration of outside functions
+ostream& operator<< (const ostream&, fraction&);
 
+
+//construction functions
+fraction::fraction(LL up, LL down) {
+	molecular = up;
+	denominator = down;
+	reduction();
+}
+fraction::fraction(double val) {
+	int ti=val;
+	double td = val-ti;
+	int times = 1;
+	while (td > 0){
+		val *= 10;
+		ti   = val;
+		td   = val - ti;
+		times *=10; 
+	}
+	molecular   = ti;
+	denominator = times;
+	reduction();
+}
+
+//reduction important!
+
+void fraction::reduction() {
+	int span = gcd(molecular,denominator);
+	if(span != 0){
+		molecular   = molecular/span;
+		denominator = denominator/span;
+	}
+}
+
+//comperation operators
+
+bool fraction::operator== (const fraction f) {
+	return this->val() == f.val();
+}
+bool fraction::operator<  (const fraction f) {
+	return this->val() < f.val();
+}
+bool fraction::operator>  (const fraction f) {
+	return this->val() > f.val();
+}
+
+//other operators
+
+
+fraction fraction::operator+ (const fraction &c) {
+	fraction b = 0;
+	b.denominator = this->denominator*c.denominator;
+	b.molecular   = this->molecular*c.denominator+c.molecular*this->denominator;
+	b.reduction();
+	return b;
+}
+fraction fraction::operator- (const fraction &c) {
+	fraction b = 0;
+	b.denominator = this->denominator*c.denominator;
+	b.molecular   = this->molecular*c.denominator-c.molecular*this->denominator;
+	b.reduction();
+	return b;
+}
+fraction fraction::operator* (const fraction &c) {
+	fraction b = 0;
+	b.denominator = this->denominator*c.denominator;
+	b.molecular   = this->molecular*c.molecular;
+	b.reduction();
+	return b;
+}
+fraction fraction::operator/ (const fraction &c) {
+	fraction b = 0;
+	fraction temp;
+	temp.molecular = c.denominator;
+	temp.denominator = c.molecular;
+	b=*this*temp;
+	return b;
+}
+
+//output function
+ostream& operator<<(ostream& out, fraction &f) {
+	if(f.deno() == 1 || f.mole() == 0)
+		out<<f.mole();
+	else
+		out<<f.mole()<<"/"<<f.deno();
+	return out;
+}
+
+//Some other api
+template <typename _T>
+_T operator+ (_T &ori, const fraction &f) {
+	ori = ori+f.val();
+	return ori;
+}
+template <typename _T>
+_T operator- (_T &ori, const fraction &f) {
+	ori = ori-f.val();
+	return ori;
+}
+template <typename _T>
+_T operator* (_T &ori, const fraction &f) {
+	ori = ori*f.val();
+	return ori;
+}
+template <typename _T>
+_T operator/ (_T &ori, const fraction &f) {
+	ori = ori/f.val();
+	return ori;
+}
 #endif
